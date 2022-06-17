3e3dc5c4e1feaff8ea3281186896579e9cdc9b96
Richard Wolfmayr <richard.wolfmayr@gmail.com>
2022-06-17 10:49:03 +0200

    set up raw block

---
+++ lang/en/block_exacsvenrol.php
@@ -0,0 +1,5 @@
+<?php
+$string['pluginname'] = 'CSV Enrol Tool';
+$string['exacsvenrol'] = 'CSV Enrol Tool';
+$string['exacsvenrol:addinstance'] = 'Add a new exacsvenrol block';
+$string['exacsvenrol:myaddinstance'] = 'Add a new exacsvenrol block to the My Moodle page';
