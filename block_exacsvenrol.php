2010f9e6bba8878f2a99202aab798404a902ae80
Elias <ereichel@htl-steyr.ac.at>
2022-07-01 17:02:13 +0200

    user enrolment works now!

--- block_exacsvenrol.php
+++ block_exacsvenrol.php
@@ -23,26 +23,6 @@ class block_exacsvenrol extends block_base
         $this->content->text = $content;
         return $this->content;
     }
-
-    public function readCSV()
-    {
-        global $CFG, $DB;
-
-        $myfile = fopen( $CFG->fileroot . "\\blocks\\exacsvenrol\\user.csv", "r");
-        $value = fread($myfile, filesize($CFG->fileroot . "\\blocks\\exacsvenrol\\user.csv"));
-        fclose($myfile);
-
-        $users = explode("\n", $value);
-
-        foreach ($users as $u) {
-            $u = explode(",", $u);
-            $user = create_user_record($u[0], $u[4]);
-            $user->firstname = $u[1];
-            $user->lastname = $u[2];
-            $user->email = $u[3];
-            $DB->update_record('user', $user);
-        }
-    }
     // The PHP tag and the curly bracket for the class definition
     // will only be closed after there is another function added in the next section.
 }
--- import.php
+++ import.php
@@ -15,45 +15,100 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 global $ADMIN, $CFG, $DB, $PAGE;
+
+use context_system;
+use moodle_url;
+
 require_once('../../config.php');
-require_once($CFG->libdir.'/adminlib.php');
+require_once($CFG->libdir . '/adminlib.php');
 
 $PAGE->set_context(context_system::instance());
-
 $PAGE->set_url(new moodle_url('/blocks/exacsvenrol/import.php'));
 $PAGE->set_title("Import Title");
 $PAGE->set_heading("Import Heading");
+$PAGE->set_pagelayout('mydashboard');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    createUser();
+}
+
+function createUser(){
+    global $CFG, $DB;
     if (!empty($_FILES)) {
         try {
             move_uploaded_file($_FILES['file']['tmp_name'], $CFG->fileroot . "\\blocks\\exacsvenrol\\files\\" . $_FILES['file']['name']);
         } finally {
-            try{
-                $myfile = fopen( $CFG->fileroot . "\\blocks\\exacsvenrol\\files\\" . $_FILES['file']['name'], "r");
+            try {
+                $myfile = fopen($CFG->fileroot . "\\blocks\\exacsvenrol\\files\\" . $_FILES['file']['name'], "r");
                 $value = fread($myfile, filesize($CFG->fileroot . "\\blocks\\exacsvenrol\\files\\" . $_FILES['file']['name']));
+                unlink($CFG->fileroot . "\\blocks\\exacsvenrol\\files\\" . $_FILES['file']['name']);
                 fclose($myfile);
 
                 $users = explode("\n", $value);
 
                 foreach (array_filter($users) as $u) {
                     $u = explode(",", $u);
-                    if(($DB->get_record('user', ['username' => $u[0]])) !== null){
+                    if (($DB->get_record('user', ['username' => $u[0]])) !== null) {
                         $user = create_user_record($u[0], $u[4]);
                         $user->firstname = $u[1];
                         $user->lastname = $u[2];
                         $user->email = $u[3];
+
                         $DB->update_record('user', $user);
+
+                        $user = $DB->get_record('user', ['username' => $u[0]]);
+                        $course = $DB->get_record('course', ['fullname' => $u[6]]);
+                        enrolUser($user->id, strtolower($u[5]), $course->id);
                     }
                 }
-            } catch (Exception $e){
-                echo "One of the users you wanted to upload is already existing!";
+                echo "Users were created successfully!";
+            } catch (Exception $e) {
+                echo $e;
             }
         }
     } else {
-        echo "<br/> Something went wrong!";
+        echo "<br/> Couldn't find the file!";
     }
 }
+
+function enrolUser($userid, $role, $courseid)
+{
+    $manualinstance = null;
+    $enrol = enrol_get_plugin("manual");
+    $instances = enrol_get_instances($courseid, true);
+
+    foreach ($instances as $instance) {
+        if ($instance->enrol == "manual") {
+            $manualinstance = $instance;
+            break;
+        }
+    }
+
+    if ($role == "manager") {
+        $roleid = 1;
+    } elseif ($role == "coursecreator") {
+        $roleid = 2;
+    } elseif ($role == "editingteacher") {
+        $roleid = 3;
+    } elseif ($role == "teacher") {
+        $roleid = 4;
+    } elseif ($role == "student") {
+        $roleid = 5;
+    } elseif ($role == "guest") {
+        $roleid = 6;
+    } elseif ($role == "user") {
+        $roleid = 7;
+    } elseif ($role == "frontpage") {
+        $roleid = 8;
+    } else {
+        throw new Exception("There isn't a role called: " . $role);
+    }
+
+    if ($manualinstance != null) {
+        $enrol->enrol_user($manualinstance, $userid, $roleid);
+    }
+}
+
 ?>
 <h1>EXACSVENROL</h1>
 
