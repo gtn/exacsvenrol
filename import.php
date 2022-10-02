3409185b1b4c1675806f9805df9eeba045ff089a
Elias <ereichel@htl-steyr.ac.at>
2022-10-02 19:42:49 +0200

    added optional user import (email) added different enroltypes (id, shortname)

--- import.php
+++ import.php
@@ -29,81 +29,118 @@ $PAGE->set_heading(get_string("pluginname", "block_exacsvenrol"));
 $PAGE->set_pagelayout('mydashboard');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    createUser();
+    readCSV();
 }
 
 echo $OUTPUT->header();
 
-function createUser(){
-    global $CFG, $DB;
-    if (!empty($_FILES)) {
-        try {
-            move_uploaded_file($_FILES['file']['tmp_name'], $CFG->fileroot . "\\blocks\\exacsvenrol\\files\\" . $_FILES['file']['name']);
-        } finally {
-            try {
-                $myfile = fopen($CFG->fileroot . "\\blocks\\exacsvenrol\\files\\" . $_FILES['file']['name'], "r");
-                $value = fread($myfile, filesize($CFG->fileroot . "\\blocks\\exacsvenrol\\files\\" . $_FILES['file']['name']));
-                unlink($CFG->fileroot . "\\blocks\\exacsvenrol\\files\\" . $_FILES['file']['name']);
-                fclose($myfile);
+function readCSV()
+{
+    global $CFG;
+    try {
+        move_uploaded_file($_FILES['file']['tmp_name'], $CFG->fileroot . "\\blocks\\exacsvenrol\\files\\" . $_FILES['file']['name']);
+    } finally {
+        $myfile = fopen($CFG->fileroot . "\\blocks\\exacsvenrol\\files\\" . $_FILES['file']['name'], "r");
+        $value = fread($myfile, filesize($CFG->fileroot . "\\blocks\\exacsvenrol\\files\\" . $_FILES['file']['name']));
+        unlink($CFG->fileroot . "\\blocks\\exacsvenrol\\files\\" . $_FILES['file']['name']);
+        fclose($myfile);
 
-                $users = explode("\n", $value);
-                $userList = array();
-                $counter = 0;
-                $errorCounter = 0;
-                $enrollCounter = 0;
+        $info = explode("\n", $value);
+        $firstElement = $info[0];
+        array_shift($info);
 
-                foreach (array_filter($users) as $u) {
-                    $u = explode(",", $u);
-                    if (($DB->get_record('user', ['username' => $u[0]])) == null) {
-                        $user = create_user_record($u[0], "sicheresPasswort123!");
-                        $user->firstname = $u[1];
-                        $user->lastname = $u[2];
-                        $user->email = $u[3];
+        foreach (explode(",", $firstElement) as $head) {
+            if (strtolower($head) == "email" || strtolower($head) == "e-mail") {
+                createUser($info);
+            }
 
-                        $DB->update_record('user', $user);
+            if (strtolower($head) == "courseid") {
+                enrolType($info, "id");
+                break;
 
-                        $counter++;
-                    } else {
-                        $userList[] = $u[0];
-                        $errorCounter++;
-                    }
-                    $user = $DB->get_record('user', ['username' => $u[0]]);
-                    $course = $DB->get_record('course', ['shortname' => $u[5]]);
-                    $context = context_course::instance($course->id);
-
-                    if(!is_enrolled($context, $user)){
-                        enrolUser($user->id, strtolower($u[4]), $course->id);
-                        $enrollCounter++;
-                    }
-                }
-            } catch (Exception $e) {
-                $msg = $e->getMessage();
-                echo "<script>alert('$msg')</script>";
-            } finally {
-                if($counter > 0){
-                    if($counter > 1){
-                        $msg = get_string('usersCreated', 'block_exacsvenrol', $counter);
-                    } else {
-                        $msg = get_string('userCreated', 'block_exacsvenrol', $counter);
-                    }
-                    echo "<script>alert('$msg')</script>";
-                }
-                if($enrollCounter > 0){
-                    if($enrollCounter > 1){
-                        $msg = get_string('enrolledUsers', 'block_exacsvenrol', $enrollCounter);
-                    } else {
-                        $msg = get_string('enrolledUser', 'block_exacsvenrol', $enrollCounter);
-                    }
-                    echo "<script>alert('$msg')</script>";
-                }
-                if($errorCounter > 0){
-                    $msg = getErrorUserList($userList);
-                    echo "<script>alert('$msg')</script>";
-                }
+            } elseif (strtolower($head) == "courseshort") {
+                enrolType($info, "shortname");
+                break;
             }
         }
-    } else {
-        echo get_string('filenotreadable', 'block_exacsvenrol');
+    }
+}
+
+function createUser($value)
+{
+    global $DB;
+    try {
+        $counter = 0;
+        $errorCounter = 0;
+
+        foreach (array_filter($value) as $u) {
+            $u = explode(",", $u);
+            if (($DB->get_record('user', ['username' => $u[0]])) == null) {
+                $user = create_user_record($u[0], "sicheresPasswort123!");
+                $user->firstname = $u[1];
+                $user->lastname = $u[2];
+                $user->email = $u[3];
+
+                $DB->update_record('user', $user);
+
+                $counter++;
+            } else {
+                $userList[] = $u[0];
+                $errorCounter++;
+            }
+        }
+    } catch (Exception $e) {
+        $msg = $e->getMessage();
+        echo "<script>alert('$msg')</script>";
+    } finally {
+        if ($counter > 0) {
+            if ($counter > 1) {
+                $msg = get_string('usersCreated', 'block_exacsvenrol', $counter);
+            } else {
+                $msg = get_string('userCreated', 'block_exacsvenrol', $counter);
+            }
+            echo "<script>alert('$msg')</script>";
+        }
+        if ($errorCounter > 0) {
+            $msg = getErrorUserList($userList);
+            echo "<script>alert('$msg')</script>";
+        }
+    }
+}
+
+function enrolType($value, $type)
+{
+    global $DB;
+    $enrolCounter = 0;
+    foreach (array_filter($value) as $elem) {
+        $e = explode(",", $elem);
+        $user = $DB->get_record('user', ['username' => $e[0]]);
+        if ($type == "id") {
+            $course = $DB->get_record('course', ['id' => intval($e[5])]);
+            $context = context_course::instance($course->id);
+
+            if (!is_enrolled($context, $user)) {
+                enrolUser($user->id, strtolower($e[4]), $course->id);
+                $enrolCounter++;
+            }
+        } else {
+            $course = $DB->get_record('course', ['shortname' => $e[6]]);
+            $context = context_course::instance($course->id);
+
+            if (!is_enrolled($context, $user)) {
+                enrolUser($user->id, strtolower($e[4]), $course->id);
+                $enrolCounter++;
+            }
+        }
+    }
+
+    if ($enrolCounter > 0) {
+        if ($enrolCounter > 1) {
+            $msg = get_string('enrolledUsers', 'block_exacsvenrol', $enrolCounter);
+        } else {
+            $msg = get_string('enrolledUser', 'block_exacsvenrol', $enrolCounter);
+        }
+        echo "<script>alert('$msg')</script>";
     }
 }
 
@@ -137,7 +174,7 @@ function enrolUser($userid, $role, $courseid)
     } elseif ($role == "frontpage") {
         $roleid = 8;
     } else {
-        throw new Exception(get_string("roleException","block_exacsvenrol", $role));
+        throw new Exception(get_string("roleException", "block_exacsvenrol", $role));
     }
 
     if ($manualinstance != null) {
@@ -145,20 +182,23 @@ function enrolUser($userid, $role, $courseid)
     }
 }
 
-function getErrorUserList($users){
+function getErrorUserList($users)
+{
     $msg = "";
     $counter = 0;
     $userList = "";
-    foreach ($users as $u){
-        if(next($users)){
+    foreach ($users as $u) {
+        if (next($users)) {
             $userList .= $u . ", ";
-        } else {
+        } elseif ($counter > 1) {
             $userList .= "and " . $u;
+        } else {
+            $userList .= $u;
         }
         $counter++;
     }
 
-    if($counter > 1){
+    if ($counter > 1) {
         $msg = get_string('moreUsersExists', 'block_exacsvenrol', $userList);
     } else {
         $msg = get_string('userExists', 'block_exacsvenrol', $userList);
@@ -167,17 +207,17 @@ function getErrorUserList($users){
     return $msg;
 }
 
-    echo "<form method='post' enctype='multipart/form-data'>
-    ". get_string('csvhint', 'block_exacsvenrol') ."
+echo "<form method='post' enctype='multipart/form-data'>
+    " . get_string('csvhint', 'block_exacsvenrol') . "
     </label>
     <br/>
     <input name='file' type='file' accept='text/csv'>
     <br/><br/>
-    <input type='submit' value='". get_string('uploadButton', 'block_exacsvenrol')."'>
+    <input type='submit' value='" . get_string('uploadButton', 'block_exacsvenrol') . "'>
 </form>"
 ?>
 
 
 <?php
-    echo $OUTPUT->footer();
+echo $OUTPUT->footer();
 ?>
